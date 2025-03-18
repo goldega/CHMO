064a75180c4d947f695c01084c60b0111d9d6786
goldega <tatarcukegor98@gmail.com>
2025-03-18 12:03:01 +0300

    Update hw09.rs

--- src/homework/hw09.rs
+++ src/homework/hw09.rs
@@ -1,4 +1,3 @@
-// Функція для зсуву рядка
 pub fn shift_string(s: &str, n: usize) -> String {
     let len = s.len();
     
@@ -6,10 +5,8 @@ pub fn shift_string(s: &str, n: usize) -> String {
         return String::new();
     }
 
-    // Оскільки зсув більше довжини рядка повторюється, беремо залишок від ділення на довжину
     let n = n % len;
 
-    // Розділяємо рядок на дві частини та об'єднуємо їх після зсуву
     let (first, second) = s.split_at(len - n);
     format!("{}{}", second, first)
 }
